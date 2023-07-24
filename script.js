4aafac374d6b00211ebee8846c45a910aa10cc97
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-07-24 14:22:46 +0300

    Fix js errors; Run npx eslint .

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav');
 
-hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle('is-active');
-    navMenu.classList.toggle('is-active');
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  navMenu.classList.toggle('is-active');
+});
 
-document.querySelectorAll(".nav").forEach(n => n.addEventListener("click", function () {
-    hamburger.classList.remove("is-active");
-    navMenu.classList.remove("is-active");
-}))
+document.querySelectorAll('.nav').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('is-active');
+  navMenu.classList.remove('is-active');
+}));
